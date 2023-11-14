c6ba9ac282dc194b06f44172ccc3f2d69b0e35ce
jelly <huguodong1212@gmail.com>
2023-11-14 17:29:05 +0800

    完善default.go和match.go代码

--- search/default.go
+++ search/default.go
@@ -1,1 +1,16 @@
 package search
+
+// defaultMatcher 实现了默认匹配器
+type defaultMatcher struct {
+}
+
+// init 讲默认匹配器注册到程序
+func init() {
+	var matcher defaultMatcher
+	Register("default", matcher)
+}
+
+// Search 实现默认匹配器的行为
+func (m defaultMatcher) Search(feed *Feed, searchTerm string) ([]*Result, error) {
+	return nil, nil
+}
--- search/match.go
+++ search/match.go
@@ -1,1 +1,12 @@
 package search
+
+// Result 保存搜索结果
+type Result struct {
+	Field   string
+	Content string
+}
+
+// Matcher 定义要实现的新搜索类型行为
+type Matcher interface {
+	Search(feed *Feed, searchTerm string) ([]*Result, error)
+}
